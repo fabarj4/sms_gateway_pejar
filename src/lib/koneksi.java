37776f9daec61d269afa3c9af6dc54a786e8f5fb
fabarj4 <F@DESKTOP-QHG7VED>
2017-05-27 11:27:55 +0700

    SMS GateWay

---
+++ src/lib/koneksi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib;
+
+/**
+ *
+ * @author F
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+ public Connection Connect() throws ClassNotFoundException{
+        try {
+            String url="jdbc:mysql://localhost:3306/ragunan"; //url database
+            String user="root"; //user database
+            String pass=""; //password database
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url,user,pass);        
+            return conn;
+        } catch (SQLException e) {
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return null;
+    }        
+}
